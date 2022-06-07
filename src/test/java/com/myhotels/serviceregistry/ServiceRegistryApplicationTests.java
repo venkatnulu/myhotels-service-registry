308bd65262b69772b1dea0cb04a7ff6abbb37c82
VenkataSubbaiah Nulu <VenkataSubbaiah.Nulu@cantire.com>
2022-06-07 16:54:11 +0530

    Initial Commit

---
+++ src/test/java/com/myhotels/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhotels.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
